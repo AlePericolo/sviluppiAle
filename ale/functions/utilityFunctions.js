428e05aaf57d38414c380124d50638b1e7081c94
alessandro <alessandro.pericolo@dscsrl.it>
2019-01-03 14:37:00 +0100

    organizzazione progetti

--- ale/functions/utilityFunctions.js
+++
@@ -1,7 +0,0 @@
-function isEmpty(str) {
-    return (!str || 0 === str.length);
-}
-
-function round2decimal(number){
-    return parseFloat(Math.round(number * 100) / 100).toFixed(2);
-}
--- snake/utility/functions.py
+++
@@ -1,7 +0,0 @@
-def findPosition(playerPos):
-    if playerPos == '1':
-        return (255, 0, 0) #red
-    elif playerPos == '2' or playerPos == '3':
-        return (0, 255, 0) #green
-    else:
-        return (0, 255, 255) #azure

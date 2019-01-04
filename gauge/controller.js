45f5f8a434e24b99e0dc3439b6c91bede762e97f
alessandro <alessandro.pericolo@dscsrl.it>
2019-01-04 17:33:36 +0100

    correzioni gauge

--- gauge/controller.js
+++ gauge/controller.js
@@ -1,12 +1,10 @@
 var app = angular.module('app', ['angularjs-gauge']);
 
-
 app.controller('AppController', ['$scope', function($scope) {
 
     $scope.statoAvanzamento1 = 15;
     $scope.statoAvanzamento2 = 55;
     $scope.statoAvanzamento3 = 95;
-
 
     $scope.thresholdOptions = {
         '0': { color: 'red' },

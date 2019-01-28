44434e3c6c02c223e59fed59a7d9d45d11d10688
alessandro <alessandro.pericolo@dscsrl.it>
2019-01-28 09:52:15 +0100

    merge problem

--- portfolio/controller/controller.js
+++ portfolio/controller/controller.js
@@ -1,4 +1,4 @@
-var app = angular.module("dangerApp", ["ngRoute", "ngAnimate"]);
+var app = angular.module("dangerApp", ["ngRoute", "ngAnimate", "ngMaterial", "ngMessages"]);
 
 app.config(function($routeProvider) {
     $routeProvider
@@ -22,6 +22,26 @@ app.config(function($routeProvider) {
 app.controller("projectController", function ($scope) {
     $scope.msg = "PROGETTI";
 });
+
+
+
 app.controller("contactController", function ($scope) {
-    $scope.msg = "CONTATTI";
+    $scope.user = {
+        title: 'Developer',
+        email: 'ipsum@lorem.com',
+        firstName: '',
+        lastName: '',
+        company: 'Google',
+        address: '1600 Amphitheatre Pkwy',
+        city: 'Mountain View',
+        state: 'CA',
+        biography: 'Loves kittens, snowboarding, and can type at 130 WPM.\n\nAnd rumor has it she bouldered up Castle Craig!',
+        postalCode: '94043'
+    };
+
+    $scope.states = ('AL AK AZ AR CA CO CT DE FL GA HI ID IL IN IA KS KY LA ME MD MA MI MN MS ' +
+        'MO MT NE NV NH NJ NM NY NC ND OH OK OR PA RI SC SD TN TX UT VT VA WA WV WI ' +
+        'WY').split(' ').map(function(state) {
+        return {abbrev: state};
+    });
 });

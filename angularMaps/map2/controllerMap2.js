7c21525a929099e37ca2d62a5af643ad2b04d90c
alessandro <alessandro.pericolo@dscsrl.it>
2019-04-19 17:57:26 +0200

    map2 popup + icon + link

--- angularMaps/map2/controllerMap2.js
+++ angularMaps/map2/controllerMap2.js
@@ -1,84 +1,96 @@
 var mapApp = angular.module('mapApp', []);
+
 mapApp.controller('map2Controller', function ($scope) {
 
-  var mapOptions = {
-      zoom: 4,
-      center: new google.maps.LatLng(25,80),
-      mapTypeId: google.maps.MapTypeId.ROADMAP
-  };
+    $scope.init = function () {
+        $scope.caricaDati();
+    };
 
-var cities = [
-    /*
-    { id: 1, name: 'Milano', desc: '', pos: [45.4642700, 9.1895100] , icon: {url: 'img/milan.png'}},
-    { id: 1, name: 'Pisa', desc: '', pos: [43.7085300, 10.4036000] , icon: {url: 'img/milan.png'}},
-    { id: 1, name: 'Roma', desc: '', pos: [41.8919300, 12.5113300] , icon: {url: 'img/milan.png'}},
-    { id: 1, name: 'Venezia', desc: '', pos: [45.4371300, 12.3326500] , icon: {url: 'img/milan.png'}},
-    */
-    {
-        place : 'India',
-        desc : 'A country of culture and tradition!',
-        lat : 23.200000,
-        long : 79.225487
-    },
-    {
-        place : 'New Delhi',
-        desc : 'Capital of India...',
-        lat : 28.500000,
-        long : 77.250000
-    },
-    {
-        place : 'Kolkata',
-        desc : 'City of Joy...',
-        lat : 22.500000,
-        long : 88.400000
-    },
-    {
-        place : 'Mumbai',
-        desc : 'Commercial city!',
-        lat : 19.000000,
-        long : 72.90000
-    },
-    {
-        place : 'Bangalore',
-        desc : 'Silicon Valley of India...',
-        lat : 12.9667,
-        long : 77.5667
+    $scope.caricaDati = function () {
+        //instanzio la mappa e la lego al div con id 'map'
+        $scope.map = new google.maps.Map(document.getElementById('map'), {
+               zoom: 6,
+               center: new google.maps.LatLng(43, 10), //italia
+               mapTypeId: google.maps.MapTypeId.SATELLITE
+        });
+
+        $scope.geolocalizza();
+
+        //gestione marker
+        $scope.setMarker();
+    };
+
+    $scope.setMarker = function () {
+
+        $scope.cities = getCities();
+        if (!$scope.cities.length > 0) {
+            alert('Nessun marker');
+        } else {
+            $scope.markers = [];
+
+            $scope.cities.forEach(function (c) {
+                //instanzio l'oggetto infoWindow (finestra che si apre sulla mappa, contenitore)
+                infoWindow = new google.maps.InfoWindow();
+
+                //instanzio l'oggetto marker + setto i contenuti che visualizzero in infoWindow
+                var marker = new google.maps.Marker({
+                    map: $scope.map,
+                    position: new google.maps.LatLng(c.pos[0], c.pos[1]),
+                    title: c.name,
+                    icon: c.icon,
+                    content : '<div> '+ c.desc +'<br/><a href="https://it.wikipedia.org/w/index.php?title='+c.name+'" target="_blank"> more info</a> <br/><br/>Lat: ' + c.pos[0] + ' Lon:' + c.pos[1] +'</div>'
+                });
+
+                //collegon InfoWindow e Marker
+                google.maps.event.addListener(marker, 'click', function () {
+                    infoWindow.setContent('<h2>' + marker.title + '</h2>' + marker.content);
+                    infoWindow.open($scope.map, marker);
+                });
+
+                $scope.markers.push(marker);
+            });
+        }
+    };
+
+    $scope.openInfoWindow = function (e, selectedMarker) {
+        e.preventDefault();
+        google.maps.event.trigger(selectedMarker, 'click');
+    };
+
+    $scope.geolocalizza = function () {
+        infoWindow = new google.maps.InfoWindow();
+
+        // Try HTML5 geolocation.
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(function(position) {
+                var pos = {lat: position.coords.latitude,lng: position.coords.longitude};
+                infoWindow.setPosition(pos);
+                infoWindow.setContent('Location found.');
+                infoWindow.open(map);
+                $scope.map.setCenter(pos);
+            }, function() {
+                handleLocationError(true, infoWindow, $scope.map.getCenter());
+            });
+        } else {
+            // Browser doesn't support Geolocation
+            handleLocationError(false, infoWindow, $scope.map.getCenter());
+        }
+    };
+
+    function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+        infoWindow.setPosition(pos);
+        infoWindow.setContent(browserHasGeolocation ? 'Error: The Geolocation service failed.' : 'Error: Your browser doesn\'t support geolocation.');
+        infoWindow.open($scope.map);
     }
 
-];
-
-  $scope.map = new google.maps.Map(document.getElementById('map'), mapOptions);
-
-  $scope.markers = [];
-
-  var infoWindow = new google.maps.InfoWindow();
-
-  var createMarker = function (info){
-
-      var marker = new google.maps.Marker({
-          map: $scope.map,
-          position: new google.maps.LatLng(info.lat, info.long),
-          title: info.place
-      });
-      marker.content = '<div class="infoWindowContent">' + info.desc + '<br />' + info.lat + ' E,' + info.long +  ' N, </div>';
-
-      google.maps.event.addListener(marker, 'click', function(){
-          infoWindow.setContent('<h2>' + marker.title + '</h2>' +
-            marker.content);
-          infoWindow.open($scope.map, marker);
-      });
-
-      $scope.markers.push(marker);
-
-  }
-
-  for (i = 0; i < cities.length; i++){
-      createMarker(cities[i]);
-  }
-
-  $scope.openInfoWindow = function(e, selectedMarker){
-      e.preventDefault();
-      google.maps.event.trigger(selectedMarker, 'click');
-  }
-
 });
+
+function getCities() {
+    return [
+        {id: 1, name: 'Milano', desc: "Milano &eacute; un comune italiano capoluogo della regione Lombardia, dell'omonima citt&aacute; metropolitana, e centro di una delle pi$uacute; popolose aree metropolitane d'Europa.", pos: [45.4642700, 9.1895100], icon: {url: 'img/milan.png'}},
+        {id: 1, name: 'Pisa', desc: "Pisa &eacute; un comune italiano capoluogo della provincia omonima in Toscana.", pos: [43.7085300, 10.4036000], icon: {url: 'img/pisa.png'}},
+        {id: 1, name: 'Roma', desc: "Roma, capitale della Repubblica Italiana, nonch&eacute; capoluogo dell'omonima citt&aacute; metropolitana e della regione Lazio. La citt&aacute; &eacute; dotata di un ordinamento amministrativo speciale, denominato Roma Capitale, disciplinato da una legge dello Stato.", pos: [41.8919300, 12.5113300], icon: {url: 'img/rome.png'}},
+        {id: 1, name: 'Venezia', desc: "Venezia &eacute; un comune italiano capoluogo dell'omonima citt&aacute; metropolitana e della regione Veneto.", pos: [45.4371300, 12.3326500], icon: {url: 'img/venice.png'}}
+    ];
+}
+
